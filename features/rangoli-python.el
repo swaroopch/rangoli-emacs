2a61a9b683aa4dee87c9146489e974081fa09c50
Swaroop Chitlur <swaroop@helpshift.com>
2019-08-26 16:17:27 -0700

    [python] Use microsoft's python language server

--- features/rangoli-python.el
+++ features/rangoli-python.el
@@ -1,19 +1,18 @@
 ;; rangoli-python.el --- python programming functionality -*- lexical-binding: t; -*-
 
-;;; Packages
-
-(straight-use-package 'pythonic)
+;;; LSP
+(straight-use-package 'lsp-python-ms)
+;; TODO https://github.com/microsoft/python-language-server/blob/master/TROUBLESHOOTING.md#unresolved-import-warnings
+(require 'lsp-python-ms)
 
 ;;; pyenv
 (require 'python)
-(require 'lsp-pyls)
 (when (f-directory? (f-expand "~/.pyenv/shims/"))
   (add-to-list 'python-shell-exec-path (f-expand "~/.pyenv/shims/"))
   (setq flycheck-python-pycompile-executable  (f-expand "~/.pyenv/shims/python")
         flycheck-python-flake8-executable (f-expand "~/.pyenv/shims/python")
         flycheck-python-pylint-executable (f-expand "~/.pyenv/shims/python")
-        flycheck-python-mypy-executable (f-expand "~/.pyenv/shims/mypy")
-        lsp-pyls-server-command (f-expand "~/.pyenv/shims/pyls")))
+        flycheck-python-mypy-executable (f-expand "~/.pyenv/shims/mypy")))
 
 ;;; python3, when not using pyenv
 (when (not (f-directory? (f-expand "~/.pyenv/shims/")))
@@ -22,21 +21,16 @@
         flycheck-python-flake8-executable "python3"
         flycheck-python-pylint-executable "python3"))
 
-;;; lsp
-(setq lsp-pyls-plugins-pylint-enabled nil
-      lsp-pyls-plugins-pycodestyle-max-line-length 120)
+;;; DAP
+(require 'dap-python)
 
 ;;; virtualenv
+(straight-use-package 'pythonic)
 (add-hook 'python-mode-hook
           (lambda ()
             (rangoli/declare-prefix-for-mode "v" "virtualenv")
             (rangoli/set-local-leader-key "v a" 'pythonic-activate "activate")
             (rangoli/set-local-leader-key "v d" 'pythonic-deactivate "deactivate")))
-
-;;; DAP
-
-;; https://www.reddit.com/r/emacs/comments/bxhod6/where_do_i_find_templates_for_dapmode/eq6rm65
-(require 'dap-python)
 
 (provide 'rangoli-python)
 ;; rangoli-python.el ends here
