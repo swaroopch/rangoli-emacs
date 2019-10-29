a23a1d785e16d7bab73c8cf59e5fe66cc9b40b28
Swaroop Chitlur <swaroop@swaroopch.com>
2019-10-29 05:15:07 -0700

    [bookmarks] enable multiple bookmark files

--- features/swa-bookmarks.el
+++ features/swa-bookmarks.el
@@ -2,11 +2,10 @@
 
 ;; Inspired by https://github.com/emacs-helm/helm/blob/master/helm-org.el
 
-(defvar swa/bookmarks-file
-  (-first #'f-exists?
-          (list
-           (f-join rangoli/work-dir "bookmarks.org")
-           (f-join rangoli/home-dir "bookmarks.org"))))
+(defvar swa/bookmarks-files
+  (list
+   (f-join rangoli/work-dir "bookmarks.org")
+   (f-join rangoli/home-dir "bookmarks.org")))
 
 (defun swa/counsel-org-get-heading (&optional no-tags no-todo no-priority no-comment)
   ;; Parse link, if any, and return description inside link
@@ -23,11 +22,12 @@
 (defun swa/open-web-bookmark ()
   (interactive)
   ;; Inspired by `counsel-outline'
-  (with-current-buffer (find-file-noselect swa/bookmarks-file)
+  (with-temp-buffer
+    (org-mode)
+    (-each-r swa/bookmarks-files #'insert-file-contents)
     (goto-char (point-min))
     (ivy-read "Outline: " (counsel-outline-candidates (list :outline-title #'swa/counsel-org-get-heading))
-              :action #'swa/counsel-org-open-bookmark-action
-              )))
+              :action #'swa/counsel-org-open-bookmark-action)))
 
 (rangoli/set-leader-key "m" 'swa/open-web-bookmark)
 
